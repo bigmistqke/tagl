a784b458fdf532961fdbb73f1c8fd890c67a6a85
bigmistqke <vandijckv@gmail.com>
2024-03-12 14:13:50 +0100

    core: add array to reference-count for faster iteration

--- src/core/data-structures/reference-count.ts
+++ src/core/data-structures/reference-count.ts
@@ -1,8 +1,12 @@
 export class ReferenceCount<TData> {
-  map: Map<TData, number> = new Map()
+  private map: Map<TData, number> = new Map()
+  private array: TData[] = []
   add(data: TData) {
     const count = this.map.get(data)
     this.map.set(data, count ? count + 1 : 1)
+    if (!count) {
+      this.array = Array.from(this.map.keys())
+    }
     return () => this.delete(data)
   }
   delete(data: TData) {
@@ -11,13 +15,14 @@ export class ReferenceCount<TData> {
 
     if (count === 1) {
       this.map.delete(data)
+      this.array = Array.from(this.map.keys())
     } else {
       this.map.set(data, count - 1)
     }
   }
   forEach(callback: (data: TData) => void) {
-    for (const value of this.map.keys()) {
-      callback(value)
+    for (let i = 0; i < this.array.length; i++) {
+      callback(this.array[i]!)
     }
   }
 }
